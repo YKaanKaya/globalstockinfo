465d370dbda2f34effeaa5b684058eebf89a2b66
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-23 23:11:12 +0200

    Update globalstock.py

--- globalstock.py
+++ globalstock.py
@@ -75,35 +75,30 @@ def map_esg_risk_to_level(score):
         return "Severe"
 
 def display_esg_data_table(selected_symbols, esg_data_list):
-    # Convert the list of dictionaries to a DataFrame
     esg_df = pd.DataFrame(esg_data_list)
-    esg_df.insert(0, 'Ticker', selected_symbols)  # Add a column for tickers at the beginning
+    esg_df.insert(0, 'Ticker', selected_symbols)
     st.write("### ESG Data Table:")
     st.table(esg_df)
-        
-# Function to display ESG risk levels
+
 def display_risk_levels(tickers, esg_scores):
     st.write("### ESG Risk Levels:")
-    
+
     risk_levels = ["Very Low", "Low", "Medium", "High", "Severe"]
-    score_ranges = [5, 15, 25, 35, 45]  # Midpoint of each score range for plotting
-    colors = ["#FFEDCC", "#FFDB99", "#FFC266", "#FF9900", "#FF6600"]  # Shades of orange from light to dark
-    
-    # Create a DataFrame for plotting
+    score_ranges = [5, 15, 25, 35, 45]
+    colors = ["#FFEDCC", "#FFDB99", "#FFC266", "#FF9900", "#FF6600"]
+
     df = pd.DataFrame({
         'Risk Level': risk_levels,
         'Score Range': score_ranges,
         'Color': colors
     })
-    
-    # Create a bar chart
+
     fig = px.bar(df, x='Score Range', y='Risk Level', color='Color', orientation='h',
                  color_discrete_map=dict(zip(df['Color'], df['Color'])))
-    
-    # Annotate the chart with the scores of all selected tickers
+
     for ticker, score in zip(tickers, esg_scores):
         score_position = risk_levels.index(map_esg_risk_to_level(score))
-        annotation_x = df.loc[score_position, 'Score Range'] - 3  # Adjusted for visibility
+        annotation_x = df.loc[score_position, 'Score Range'] - 3
         fig.add_annotation(
             x=annotation_x,
             y=risk_levels[score_position],
@@ -112,8 +107,7 @@ def display_risk_levels(tickers, esg_scores):
             font=dict(color='black', size=12),
             xshift=10
         )
-    
-    # Update chart aesthetics
+
     fig.update_layout(
         title="ESG Risk Levels and Ticker Scores",
         xaxis_title="Score Range",
@@ -123,11 +117,13 @@ def display_risk_levels(tickers, esg_scores):
         paper_bgcolor='#2e2e2e',
         font=dict(color='white')
     )
-    
+
     st.plotly_chart(fig)
 
 def display_time_series_chart(symbol_data, selected_symbols, start_date, end_date):
     try:
+        # Ensure Datetime column is a datetime dtype
+        symbol_data['Datetime'] = pd.to_datetime(symbol_data.index)
         filtered_data = symbol_data[
             (symbol_data['Symbol'].isin(selected_symbols)) &
             (symbol_data['Datetime'].dt.date >= start_date) &
@@ -137,18 +133,13 @@ def display_time_series_chart(symbol_data, selected_symbols, start_date, end_dat
         if filtered_data.empty:
             st.error("No data available for the selected symbols in the selected date range.")
         else:
-            selected_tickers = ', '.join(selected_symbols)  # Join selected tickers with commas
-            
-            # Create a Plotly line chart
-            fig = go.Figure()  # Create a new Plotly figure
-            
-            # Customize the chart with explicit light colors
-            light_colors = ['#FF5733', '#FFBD33', '#33FF57', '#339CFF', '#FF33D1']  # Light colors
+            selected_tickers = ', '.join(selected_symbols)
+            fig = go.Figure()
+            light_colors = ['#FF5733', '#FFBD33', '#33FF57', '#339CFF', '#FF33D1']
             color_mapping = {symbol: light_colors[i % len(light_colors)] for i, symbol in enumerate(selected_symbols)}
-            
+
             for symbol in selected_symbols:
                 symbol_data = filtered_data[filtered_data['Symbol'] == symbol]
-                # Add trace only once for each unique symbol
                 if symbol not in fig.data:
                     fig.add_trace(
                         go.Scatter(
@@ -159,71 +150,61 @@ def display_time_series_chart(symbol_data, selected_symbols, start_date, end_dat
                             line=dict(color=color_mapping[symbol], width=2)
                         )
                     )
-                
-                # Add annotations for highest and lowest trading prices
-                min_return_row = symbol_data.loc[symbol_data['Low'].idxmin()]  # Get the row with the minimum 'Low' value
-                max_return_row = symbol_data.loc[symbol_data['High'].idxmax()]  # Get the row with the maximum 'High' value
-                
+
+                min_return_row = symbol_data.loc[symbol_data['Low'].idxmin()]
+                max_return_row = symbol_data.loc[symbol_data['High'].idxmax()]
+
                 fig.add_annotation(
                     x=min_return_row['Datetime'],
                     y=min_return_row['Low'],
-                    text=f"Lowest: ${min_return_row['Low']:.2f}",
-                    showarrow=False,
-                    arrowhead=4,
+                    text=f"Lowest: {min_return_row['Low']:.2f}",
+                    showarrow=True,
+                    arrowhead=2,
                     ax=0,
-                    ay=-40
+                    ay=40,
+                    bgcolor='white',
+                    font=dict(color=color_mapping[symbol])
                 )
-                
                 fig.add_annotation(
                     x=max_return_row['Datetime'],
                     y=max_return_row['High'],
-                    text=f"Highest: ${max_return_row['High']:.2f}",
-                    showarrow=False,
-                    arrowhead=4,
+                    text=f"Highest: {max_return_row['High']:.2f}",
+                    showarrow=True,
+                    arrowhead=2,
                     ax=0,
-                    ay=40
+                    ay=-40,
+                    bgcolor='white',
+                    font=dict(color=color_mapping[symbol])
                 )
-            
-            # Set chart title
+
             fig.update_layout(
-                title=f"Time Series Chart for {selected_tickers} Tickers",
+                title=f"Time Series Data for {selected_tickers}",
                 xaxis_title="Date",
-                yaxis_title="Highest Price"
+                yaxis_title="Value",
+                showlegend=True,
+                plot_bgcolor='#2e2e2e',
+                paper_bgcolor='#2e2e2e',
+                font=dict(color='white'),
+                legend_title_text='Symbols',
+                legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1)
             )
-            
-            # Show the chart
+
             st.plotly_chart(fig)
     except Exception as e:
         st.error(f"An error occurred: {e}")
+
 def main():
-    st.title("Stock Data Downloader & ESG Analyzer 📈")
+    st.title("Financial Data Application")
 
-    st.sidebar.header("Settings")
+    tickers_input = st.sidebar.text_input("Enter the tickers (comma separated):", "AAPL, GOOGL")
+    tickers = [ticker.strip().upper() for ticker in tickers_input.split(',')]
+    period = st.sidebar.selectbox("Select Time Period:", ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"])
+    interval = st.sidebar.selectbox("Select Time Interval:", ["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"])
+    display_esg = st.sidebar.checkbox("Display ESG data", True)
+    display_esg_risk_levels = st.sidebar.checkbox("Display ESG risk levels", True)
+    display_time_series_data = st.sidebar.checkbox("Display Time Series Data", True)
+    download_link = st.sidebar.checkbox("Download Data as CSV", False)
 
-    # Ticker input
-    default_tickers = "AAPL"
-    tickers_input = st.sidebar.text_input("Enter Stock Tickers:", default_tickers).upper()
-    tickers = tickers_input.split()
-    st.sidebar.text("Example: AAPL GOOGL MSFT")
-
-    # Period selection
-    available_periods = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"]
-    period = st.sidebar.selectbox("Select Period:", available_periods, index=2)
-
-    # Interval selection
-    available_intervals = ["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"]
-    interval = st.sidebar.selectbox("Select Interval:", available_intervals, index=8)
-
-    # Additional data features
-    st.sidebar.subheader("Additional Data Features")
-    add_cumulative_return = st.sidebar.checkbox('Add Cumulative Return', value=True)
-    add_moving_averages = st.sidebar.checkbox('Add Moving Averages', value=True)
-    add_esg_scores = st.sidebar.checkbox('Add ESG Scores and Risk Levels', value=True)
-
-    st.sidebar.text("Note: Data is fetched from Yahoo Finance.")
-    st.sidebar.text("© 2023 Your Company Name")
-
-    # Fetch and display data upon any change in settings or upon hitting "Enter" after entering a ticker
     if tickers_input != default_tickers or st.sidebar.checkbox("Refresh Data"):
         with st.spinner("Fetching data..."):
             try:
@@ -231,37 +212,35 @@ def main():
                 esg_data_list = []
                 for ticker in tickers:
                     data = yf.download(ticker, period=period, interval=interval)
-                    
-                    if add_cumulative_return:
-                        data = compute_cumulative_return(data)
-                    
-                    if add_moving_averages:
-                        data = compute_moving_averages(data)
+                    if data.empty:
+                        st.error(f"No data available for {ticker} in the selected date range.")
+                        continue
 
-                    data["Symbol"] = ticker
+                    data['Symbol'] = ticker
+                    data = compute_cumulative_return(data)
+                    data = compute_moving_averages(data)
                     data_frames.append(data)
 
-                    if add_esg_scores:
+                    if display_esg or display_esg_risk_levels:
                         esg_data = get_esg_data_with_headers_and_error_handling(ticker)
                         esg_data_list.append(esg_data)
 
                 final_data = pd.concat(data_frames)
 
-                if add_esg_scores and esg_data_list:
+                if display_esg:
                     display_esg_data_table(tickers, esg_data_list)
-                    esg_scores = [data["Total ESG risk score"] for data in esg_data_list if data["Total ESG risk score"] is not None]
+
+                if display_esg_risk_levels:
+                    esg_scores = [data["Total ESG risk score"] for data in esg_data_list]
                     display_risk_levels(tickers, esg_scores)
 
-                # Display Time Series Chart
-                display_time_series_chart(final_data, tickers, final_data["Date"].min(), final_data["Date"].max())
+                if display_time_series_data:
+                    display_time_series_chart(final_data, tickers, final_data['Datetime'].min(), final_data['Datetime'].max())
 
-                # Allow user to download the data
-                csv = final_data.to_csv(index=True)
-                b64 = b64encode(csv.encode()).decode()
-                filename = "_".join(tickers) + f"_{period}_{interval}.csv"
-                href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">📥 Download CSV File</a>'
-                st.markdown(href, unsafe_allow_html=True)
-
+                if download_link:
+                    csv = final_data.to_csv(index=False)
+                    b64 = b64encode(csv.encode()).decode()
+                    st.markdown(f"### Download Data as CSV:\n[Download Link](data:file/csv;base64,{b64})")
             except Exception as e:
                 st.error(f"An error occurred: {str(e)}")
 
