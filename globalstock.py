0a56df7527521467c8cbf6c9fcf758c88891a718
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-23 17:19:30 +0200

    Update globalstock.py

--- globalstock.py
+++ globalstock.py
@@ -9,34 +9,30 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import base64
 
-
 def download_stock_data(ticker="AAPL", period="1d", interval="1m"):
-    """
-    Downloads stock data based on the provided ticker, period, and interval.
-
-    :param ticker: Stock ticker. Default is "AAPL" for Apple.
-    :param period: Data period. Default is "1d".
-    :param interval: Data interval. Default is "1m".
-    :return: Stock data in a DataFrame.
-    """
     stock_data = yf.download(ticker, period=period, interval=interval)
     return stock_data
 
 def save_to_csv(dataframe, filename):
-    """
-    Saves the provided DataFrame to a CSV file.
-
-    :param dataframe: Data to save.
-    :param filename: Name of the CSV file.
-    """
     dataframe.to_csv(filename)
     print(f"Data saved to {filename}.")
+
+def get_valid_input(prompt, accepted_values):
+    user_input = input(prompt).lower()
+    while user_input not in accepted_values:
+        print(f"Invalid input. Accepted values: {', '.join(accepted_values)}")
+        user_input = input(prompt).lower()
+    return user_input
 
 if __name__ == "__main__":
     # 1. User Input
     ticker = input("Enter the stock ticker (default: AAPL): ") or "AAPL"
-    period = input("Enter the data period (default: 1d): ") or "1d"
-    interval = input("Enter the data interval (default: 1m): ") or "1m"
+
+    valid_periods = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"]
+    period = get_valid_input("Enter the data period (default: 1d): ", valid_periods) or "1d"
+
+    valid_intervals = ["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"]
+    interval = get_valid_input("Enter the data interval (default: 1m): ", valid_intervals) or "1m"
 
     # 2. Download Data
     data = download_stock_data(ticker, period, interval)
