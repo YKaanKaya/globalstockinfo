8cabdd2e25418e38105408e027303f731962a6a5
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-23 17:16:15 +0200

    Update globalstock.py

--- globalstock.py
+++ globalstock.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import base64
 
+
 def download_stock_data(ticker="AAPL", period="1d", interval="1m"):
     """
     Downloads stock data based on the provided ticker, period, and interval.
@@ -21,12 +22,30 @@ def download_stock_data(ticker="AAPL", period="1d", interval="1m"):
     stock_data = yf.download(ticker, period=period, interval=interval)
     return stock_data
 
+def save_to_csv(dataframe, filename):
+    """
+    Saves the provided DataFrame to a CSV file.
+
+    :param dataframe: Data to save.
+    :param filename: Name of the CSV file.
+    """
+    dataframe.to_csv(filename)
+    print(f"Data saved to {filename}.")
+
 if __name__ == "__main__":
+    # 1. User Input
     ticker = input("Enter the stock ticker (default: AAPL): ") or "AAPL"
     period = input("Enter the data period (default: 1d): ") or "1d"
     interval = input("Enter the data interval (default: 1m): ") or "1m"
 
-    raw_data = download_stock_data(ticker, period, interval)
-    
-    # Display or use the final_data as needed
-    print(final_data)
+    # 2. Download Data
+    data = download_stock_data(ticker, period, interval)
+
+    # 3. Display Data
+    print(data)
+
+    # 4. Option to Save
+    save_option = input("Do you want to save the data to a CSV? (yes/no): ").lower()
+    if save_option == "yes":
+        filename = f"{ticker}_data.csv"
+        save_to_csv(data, filename)
