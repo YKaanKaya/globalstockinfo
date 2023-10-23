8f82c9724dff5c8298784d52f78a16eb421293b4
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-23 22:09:34 +0200

    Update globalstock.py

--- globalstock.py
+++ globalstock.py
@@ -13,7 +13,7 @@ import ticker_fetcher
 # Function to download stock data using yfinance
 def download_stock_data(selected_tickers, period, interval):
     try:
-        data = yf.history(selected_tickers, period=period, interval=interval)
+        data = yf.Tickers(selected_tickers, period=period, interval=interval)
         return data
     except Exception as e:
         st.error(f"Error downloading data: {e}")
