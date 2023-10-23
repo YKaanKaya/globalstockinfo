50f7a86983fc162f4f99d61d0e366f0551590956
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-23 22:18:31 +0200

    Update globalstock.py

--- globalstock.py
+++ globalstock.py
@@ -1,38 +1,62 @@
+import yfinance as yf
 import streamlit as st
 import pandas as pd
-import yfinance as yf
-from datetime import datetime, timedelta
-import requests
-from bs4 import BeautifulSoup
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-import plotly.express as px
-import base64
-import ticker_fetcher
+
+def compute_cumulative_return(data):
+    data['Cumulative Return'] = (1 + data['Adj Close'].pct_change()).cumprod()
+    return data
+
+def compute_moving_averages(data, windows=[50, 200]):
+    for window in windows:
+        data[f'MA{window}'] = data['Adj Close'].rolling(window=window).mean()
+    return data
 
 def main():
     st.title("Stock Data Downloader")
 
-    # User input for stock ticker and period
-    ticker = st.text_input("Enter Stock Ticker:", "").upper()
+    # Descriptive text for multi-selection
+    st.write("Enter stock tickers separated by spaces. Example: AAPL GOOGL MSFT")
+
+    # User input for stock ticker (default to AAPL) and period
+    tickers = st.text_input("Enter Stock Tickers:", "AAPL").upper().split()
     available_periods = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"]
     period = st.selectbox("Select Period:", available_periods, index=2)  # default to "1mo"
 
+    # Options for cumulative return and moving averages
+    add_cumulative_return = st.checkbox('Add Cumulative Return')
+    add_moving_averages = st.checkbox('Add Moving Averages')
+
     if st.button("Download"):
-        if not ticker:
-            st.warning("Please enter a ticker.")
-        else:
-            try:
+        try:
+            data_frames = []
+            for ticker in tickers:
                 data = yf.download(ticker, period=period)
-                st.write(data)
                 
-                # Allow user to download the data
-                csv = data.to_csv(index=True)
-                b64 = b64encode(csv.encode()).decode()  # B64 encoding for downloading
-                href = f'<a href="data:file/csv;base64,{b64}" download="{ticker}_{period}.csv">Download CSV File</a>'
-                st.markdown(href, unsafe_allow_html=True)
-            except Exception as e:
-                st.error(f"An error occurred: {str(e)}")
+                if add_cumulative_return:
+                    data = compute_cumulative_return(data)
+                
+                if add_moving_averages:
+                    data = compute_moving_averages(data)
+
+                data_frames.append(data)
+
+            # Concatenate all dataframes for multiple tickers
+            if len(data_frames) > 1:
+                final_data = pd.concat(data_frames, keys=tickers, names=['Ticker', 'Date'])
+            else:
+                final_data = data_frames[0]
+
+            st.write(final_data)
+
+            # Allow user to download the data
+            csv = final_data.to_csv(index=True)
+            b64 = b64encode(csv.encode()).decode()  # B64 encoding for downloading
+            filename = "_".join(tickers) + f"_{period}.csv"
+            href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download CSV File</a>'
+            st.markdown(href, unsafe_allow_html=True)
+
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
     from base64 import b64encode
