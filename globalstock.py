c0b443e5d09d06a9a865e82a2dc2a1990d77b089
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-23 17:08:31 +0200

    Create globalstock.py

---
+++ globalstock.py
@@ -0,0 +1,45 @@
+import yfinance as yf
+
+def download_stock_data(ticker="AAPL", period="1d", interval="1m"):
+    """
+    Downloads stock data based on the provided ticker, period, and interval.
+
+    :param ticker: Stock ticker. Default is "AAPL" for Apple.
+    :param period: Data period. Default is "1d".
+    :param interval: Data interval. Default is "1m".
+    :return: Stock data in a DataFrame.
+    """
+    stock_data = yf.download(ticker, period=period, interval=interval)
+    return stock_data
+
+def process_data(dataframe):
+    """
+    Processes the stock data. This function can be extended based on specific requirements.
+
+    :param dataframe: Stock data.
+    :return: Processed data.
+    """
+    # Here, add your data processing logic if any
+    return dataframe
+
+def merge_additional_info(dataframe):
+    """
+    Merges additional info into the stock data. This function can be extended based on specific requirements.
+
+    :param dataframe: Stock data.
+    :return: Data with additional information.
+    """
+    # Add logic to merge additional info
+    return dataframe
+
+if __name__ == "__main__":
+    ticker = input("Enter the stock ticker (default: AAPL): ") or "AAPL"
+    period = input("Enter the data period (default: 1d): ") or "1d"
+    interval = input("Enter the data interval (default: 1m): ") or "1m"
+
+    raw_data = download_stock_data(ticker, period, interval)
+    processed_data = process_data(raw_data)
+    final_data = merge_additional_info(processed_data)
+
+    # Display or use the final_data as needed
+    print(final_data)
