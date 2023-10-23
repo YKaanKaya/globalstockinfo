ffd7bb456d1e1e1c835b529c2fec043ecaae8985
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-23 21:00:09 +0200

    Update globalstock.py

--- globalstock.py
+++ globalstock.py
@@ -289,8 +289,6 @@ if custom_ticker and custom_ticker not in selected_tickers:
 # Period selection
 period = st.sidebar.selectbox("Select Period", ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'], index=0)
 
-if 'selected_interval' not in st.session_state:
-    st.session_state.selected_interval = '1m'  # Set a default interval
 
 # Create the interval selection widget and update st.session_state.selected_interval when changed
 selected_interval = st.sidebar.selectbox("Select Interval", ['1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo'], index=0, key="selected_interval")
