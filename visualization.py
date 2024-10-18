2ab4c13d93f7b008c07aceebc456766216bce66e
YKKaya <73110464+YKKaya@users.noreply.github.com>
2024-10-18 12:55:56 +0200

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from utils import format_large_number
 
 def display_stock_chart(data, ticker):
     """Display stock price chart with moving averages and volume."""
